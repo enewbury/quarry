8d34d9c4c3a7fa273143e38d4f0593f7da6cfc4f
Eric Newbury <eric@newbury.cloud>
2022-04-04 19:11:24 -0400

    add coverage

--- test/quarry/utils_test.exs
+++ test/quarry/utils_test.exs
@@ -4,13 +4,23 @@ defmodule Quarry.UtilsTest do
   import Quarry.Utils
 
   describe "access_keyword/2" do
+    test "can get deep keyword values" do
+      path = [access_keyword(:first, []), access_keyword(:second, :default)]
+      assert :hello = get_in([first: [second: :hello]], path)
+      assert :default = get_in([first: []], path)
+    end
+
     test "can deep insert keyword values" do
       expected = [first: [new: [], second: []]]
+      path = [access_keyword(:first, []), access_keyword(:new, [])]
+      assert expected == put_in([first: [second: []]], path, [])
+    end
 
-      actual =
-        put_in([first: [second: []]], [access_keyword(:first, []), access_keyword(:new, [])], [])
-
-      assert expected == actual
+    test "can deep delete keyword values" do
+      path = [access_keyword(:first, []), access_keyword(:second, :default)]
+      assert {:hello, [first: []]} = pop_in([first: [second: :hello]], path)
+      assert {:default, [first: []]} = pop_in([first: []], path)
+      assert {:default, [first: []]} = pop_in([], path)
     end
   end
 end
