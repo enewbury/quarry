f97fd9689c9eb813bfa76ceb70263da8439d5e55
Eric Newbury <eric@newbury.cloud>
2022-04-04 22:36:30 -0400

    Update CI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,20 @@
 # Changelog
+## Upcoming
 
-## v0.2.0
- - Include operators like <,>, starts_with etc.
+<!-- next-header -->
 
-## v0.1.0
+## [Unreleased] - ReleaseDate
+### Added
+* Preserve errors when using Quarry.build vs Quarry.build!
+* CI updates
 
-- Initial implementation of library
+### Fixed
+
+## [0.2.0] - 2021-12-25
+* Includes more filtering options like `less_than`, `greater_than`, `starts_with` etc
+
+## [0.1.0] - 2021-12-25
+* Initial release
+
+<!-- next-url -->
+[Unreleased]: https://github.com/enewbury/quarry/compare/v0.2.1...HEAD
--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,40 @@
+use Mix.Config
 
+config :version_release,
+  tag_prefix: "v",
+  hex_publish: true,
+  changelog: %{
+    creation: :manual,
+    replacements: [
+      %{
+        file: "mix.exs",
+        patterns: [%{search: ~r/@version \"(.*)\"/, replace: "@version \"{{version}}\""}]
+      },
+      %{
+        file: "README.md",
+        patterns: [
+          %{search: ~r/quarry, \"~> (.*)\"/, replace: "quarry, \"~> {{version}}\""}
+        ]
+      },
+      %{
+        file: "CHANGELOG.md",
+        type: :changelog,
+        patterns: [
+          %{search: "Unreleased", replace: "{{version}}", type: :unreleased},
+          %{search: "...HEAD", replace: "...{{tag_name}}", global: false},
+          %{search: "ReleaseDate", replace: "{{date}}"},
+          %{
+            search: "<!-- next-header -->",
+            replace: "<!-- next-header -->\n\n## [Unreleased] - ReleaseDate",
+            global: false
+          },
+          %{
+            search: "<!-- next-url -->",
+            replace:
+              "<!-- next-url -->\n[Unreleased]: https://github.com/enewbury/quarry/compare/{{tag_name}}...HEAD",
+            global: false
+          }
+        ]
+      }
+    ]
+  }
